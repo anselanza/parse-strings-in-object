f9b10307a16f7e19d8095a6253d2eb13e186539a
Stephen Buchanan <stephen.buchanan@random.studio>
2018-02-27 14:37:34 +0100

    example must have booleans as booleans or strings

--- example/example.js
+++ example/example.js
@@ -7,9 +7,14 @@ let before = {
         anotherNumber: '3.17',
     },
     bar: {
-        active: false,
+        active: 'false',
         number: '10',
-        aString: 'yo'
+        aString: 'yo',
+        subSub: {
+            thisIsTrue: 'true',
+            thisIsFalse: 'false',
+            thisIsNumber: '0.00006'
+        }
     },
     justAString: 'hello',
     ipAddress: '192.168.1.101'
