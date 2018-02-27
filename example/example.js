213fa0ebecb2151c4b3cdcf45f43791910a753da
Stephen Buchanan <stephen.buchanan@random.studio>
2018-02-27 13:50:27 +0100

    ip addresses retained as strings

--- example/example.js
+++ example/example.js
@@ -1,7 +1,9 @@
 let before = {
     active: 'true',
     number: '2',
-    anotherNumber: '3.17'
+    anotherNumber: '3.17',
+    justAString: 'hello',
+    ipAddress: '192.168.1.101'
 }
 
 let after = require('parse-strings-in-object')(before);
