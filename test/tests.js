385904b357f8400a11695c7f17108ba897cbb9ff
Stephen Buchanan <stephen.buchanan@random.studio>
2018-03-01 16:25:01 +0100

    chai for tests

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,4 @@
-const expect = require('expect.js');
+const expect = require('chai').expect;
 const parser = require('../lib/index.js');
 
 
@@ -8,54 +8,94 @@ describe('Parsing', () => {
     it('should convert "true" and "false" to boolean values', () => {
         const before = { foo: 'true', bar: 'false' };
         const result = parser(before);
-        expect(result).to.eql({ foo: true, bar: false });
+        // expect(result).to.equal({ foo: true, bar: false });
+        expect(result.foo).to.equal(true);
+        expect(result.bar).to.equal(false);
     });
 
-    it ('should parse a nested structure properly', () => {
-        const before = {
-            topLevel: true,
-            topNumber: 1,
-            foo: {
-                active: 'true',
-                number: '0',
-                anotherNumber: '3.17',
-            },
-            bar: {
-                active: 'false',
-                number: '10',
-                aString: 'yo',
-                subSub: {
-                    thisIsTrue: 'true',
-                    thisIsFalse: 'false',
-                    thisIsNumber: '0.00006'
-                }
-            },
-            justAString: 'hello',
-            ipAddress: '192.168.1.101'
-        }
+    it('should retain arrays properly', () => {
+        const before = { foo: 'true', list: ['one', 'two', 'three']};
         const result = parser(before);
-        const expected = {
-            topLevel: true,
-            topNumber: 1,
-            foo: {
-                active: true,
-                number: 0,
-                anotherNumber: 3.17,
-            },
-            bar: {
-                active: false,
-                number: 10,
-                aString: 'yo',
-                subSub: {
-                    thisIsTrue: true,
-                    thisIsFalse: false,
-                    thisIsNumber: 0.00006
-                }
-            },
-            justAString: 'hello',
-            ipAddress: '192.168.1.101'
-        }
-        expect(result).to.eql(expected);
-
+        expect(result).to.equal({ foo: true, list: ['one', 'two', 'three']});
     });
+
+    // it('should retain array of objects properly', () => {
+    //     const before = { 
+    //         someObjs: [
+    //             {
+    //                 id: 0,
+    //                 value: 'hello'
+    //             },
+    //             {
+    //                 id: 1,
+    //                 value: 'world'
+    //             }
+    //         ]
+    //     };
+    //     const result = parser(before);
+    //     expect(result).to.be(
+    //         { 
+    //             foo: 'true', 
+    //             someObjs: [
+    //                 {
+    //                     id: 0,
+    //                     value: 'hello'
+    //                 },
+    //                 {
+    //                     id: 1,
+    //                     value: 'world'
+    //                 }
+    //             ]
+    //         }
+    //     );
+    // });
+
+
+    // it ('should parse a nested structure properly', () => {
+    //     const before = {
+    //         topLevel: true,
+    //         topNumber: 1,
+    //         foo: {
+    //             active: 'true',
+    //             number: '0',
+    //             anotherNumber: '3.17',
+    //         },
+    //         bar: {
+    //             active: 'false',
+    //             number: '10',
+    //             aString: 'yo',
+    //             subSub: {
+    //                 thisIsTrue: 'true',
+    //                 thisIsFalse: 'false',
+    //                 thisIsNumber: '0.00006'
+    //             }
+    //         },
+    //         justAString: 'hello',
+    //         ipAddress: '192.168.1.101'
+    //     }
+    //     const result = parser(before);
+    //     const expected = {
+    //         topLevel: true,
+    //         topNumber: 1,
+    //         foo: {
+    //             active: true,
+    //             number: 0,
+    //             anotherNumber: 3.17,
+    //         },
+    //         bar: {
+    //             active: false,
+    //             number: 10,
+    //             aString: 'yo',
+    //             subSub: {
+    //                 thisIsTrue: true,
+    //                 thisIsFalse: false,
+    //                 thisIsNumber: 0.00006
+    //             }
+    //         },
+    //         justAString: 'hello',
+    //         ipAddress: '192.168.1.101'
+    //     }
+    //     expect(result).to.equal(expected);
+
+    // });
 });
